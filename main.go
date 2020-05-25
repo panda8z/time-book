8599eb619148cb114bd4430f1acc8d3823b64e1b
panda8z <panda8@126.com>
2020-05-25 18:35:11 +0800

    middleware and next function

--- main.go
+++ main.go
@@ -7,22 +7,46 @@ import (
 
 func main() {
 	app := fiber.New()
-
-	app.Get("/:name/:age?", func(c *fiber.Ctx) {
-		str := fmt.Sprintf("First route: [name]: %s [age]: %s\n", c.Params("name"), c.Params("age"))
-		c.SendString(str)
-		c.Next() // Continue the stack
+	// Match any route
+	app.Use(func(c *fiber.Ctx) {
+		fmt.Println("First middleware")
+		c.Next()
 	})
 
-	app.Get("/api/*", func(c *fiber.Ctx) {
-		str := fmt.Sprintf("Last route:  [*]: %s\n", c.Params("*"))
-		c.SendString(str)
+	// Match all routes starting with /api
+	app.Use("/api", func(c *fiber.Ctx) {
+		fmt.Println("Second middleware")
+		c.Next()
 	})
 
-	app.Get("/panda/*", func(c *fiber.Ctx) {
-		str := fmt.Sprintf("Lastest route:  [*]: %s\n", c.Params("*"))
-		c.SendString(str)
+	// GET /api/register
+	app.Get("/api/list", func(c *fiber.Ctx) {
+		fmt.Println("Last middleware")
+		c.Send("Hello, World!")
 	})
+
+	//
+	//app.Get("/:name/:age?", func(c *fiber.Ctx) {
+	//	str := fmt.Sprintf("First route: [name]: %s [age]: %s\n", c.Params("name"), c.Params("age"))
+	//	c.SendString(str)
+	//	c.Next() // Continue the stack
+	//})
+	//
+	//app.Get("/api/*", func(c *fiber.Ctx) {
+	//	str := fmt.Sprintf("Last route:  [*]: %s\n", c.Params("*"))
+	//	c.SendString(str)
+	//})
+	//
+	//app.Get("/panda/*", func(c *fiber.Ctx) {
+	//	str := fmt.Sprintf("Lastest route:  [*]: %s\n", c.Params("*"))
+	//	c.SendString(str)
+	//})
+	//
+	//// Last middleware to match anything
+	//app.Use(func(c *fiber.Ctx) {
+	//	c.SendStatus(404) // => 404 "Not Found"
+	//	c.SendString("404 not found anthing")
+	//})
 
 	app.Listen(3000)
 	// GET /api/register
