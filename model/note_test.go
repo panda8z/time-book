483605e0cfb494de1c6cf50af8be02e86cb4725b
panda8z <panda8@126.com>
2020-06-12 15:27:19 +0800

    add

--- model/note_test.go
+++ model/note_test.go
@@ -1,5 +1,8 @@
 /*
 go test -v -c -o b.test  github.com/panda8z/time-book/model
+
+./b.test -test.run ^TestDeletN$
+
 go tool test2json -t a.test -test.v -test.run ^TestAddN$
 */
 package model
@@ -9,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/panda8z/time-book/pkg/settings"
 )
 
@@ -31,4 +35,31 @@ func TestAddN(t *testing.T) {
 			t.Log("Note id is ", id)
 		}
 	}
+}
+
+// ./b.test -test.run ^TestDeletN$
+func TestDeletN(t *testing.T) {
+	n := &Note{}
+	db.Last(n)
+	err := DeleteN(n.ID)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+// ./b.test -test.run ^TestEditN$
+func TestEditN(t *testing.T) {
+	n := &Note{}
+	db.Last(n)
+	EditN(n.ID, gin.H{
+		"content": "我不是黄铜，我是青铜",
+	})
+}
+
+func TestGetN(t *testing.T) {
+	
+}
+
+func TestGetNList(t *testing.T) {
+
 }
